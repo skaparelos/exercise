8eec5b47266cf76103845acf79d7b30d947b2c08
skaparelos <skaparelos@gmail.com>
2024-11-23 13:35:45 +0200

    update userService

--- lib/services/userService.ts
+++ lib/services/userService.ts
@@ -3,6 +3,11 @@ import { User } from '@/types/users';
 import { getAllUsersQuery } from '@/lib/queries/users';
 
 export async function getAllUsers(): Promise<User[]> {
-  const { rows } = await pool.query<User>(getAllUsersQuery);
-  return rows;
+  try {
+    const { rows } = await pool.query<User>(getAllUsersQuery);
+    return rows;
+  } catch (error) {
+    console.error('Error getting all users:', error);
+    throw error;
+  }
 }
